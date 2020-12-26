7ce7274347eaa01d16e698f98f8620c672180b40
mdmuzaffer <skkhan9708@gmail.com>
2020-12-27 00:19:38 +0530

    Create Model -a resource 1

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,15 +7,11 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         //
     }
+
 
     /**
      * Show the form for creating a new resource.
